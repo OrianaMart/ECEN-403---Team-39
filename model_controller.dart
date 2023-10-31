150ab412a750f007317f607220981b8c9c17ff1a
Oriana Martin <123761900+OrianaMart@users.noreply.github.com>
2023-10-30 21:20:28 -0500

    Final version of the model_controller page
    This code creates a flutter page for image classification, where users can capture photos or select images from their gallery. The selected image is then processed using a pre-trained machine learning model, and the results are displayed on the screen.

--- model_controller.dart
+++ model_controller.dart
@@ -1,29 +1,50 @@
+//Import the needed libraries
 import 'dart:io';
-
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:flutter_tflite/flutter_tflite.dart';
 
+/*
+*Author: Oriana V Martin
+* UIN: 527008754
+* Date: October 30,2023
+*
+* Description:
+* This code creates a flutter page for image classification,
+* where users can capture photos or select images from their gallery.
+* The selected image is then processed using a pre-trained machine learning model,
+* and the results are displayed on the screen.
+* */
+
+//Define a StatefulWidget for the scanning page
 class ScanningPage extends StatefulWidget {
+  const ScanningPage({Key? key}) : super(key:key);
   @override
-  _ScanningPage createState() => _ScanningPage();
+  State<ScanningPage> createState() => _ScanningPage();
 }
 
+//Define the _ScanningPage class which extends the StatefulWidget
 class _ScanningPage extends State<ScanningPage> {
-  bool loading = true;
-  File? _image;
-  List<dynamic>? _output;
-  final imagepicker = ImagePicker();
+  //Define the state variables
+  bool loading = true; //A flag to check if the app is loading
+  File? _image; //The selected image file
+  List<dynamic>? _output; //List to store the model predictions
+  final imagepicker = ImagePicker(); //Image picker instance from the model
+  String modelPred = ''; //The prediction label from the model
+  String modelConf = ''; //The confidence of the prediction
+
 
   @override
   void initState() {
     super.initState();
+    //Load the machine learning model when the page is initialized
     loadmodel().then((value) {
       setState(() {});
     });
   }
 
+  //Function to perform image detection using the loaded model
   detectimage(File image) async {
     var prediction = await Tflite.runModelOnImage(
         path: image.path,
@@ -34,22 +55,27 @@ class _ScanningPage extends State<ScanningPage> {
 
     setState(() {
       _output = prediction;
+      modelPred = (_output![0]['label']).toString().substring(2);
+      modelConf = ((_output![0]['confidence']).toString());
       loading = false;
     });
   }
 
+  //Function to load the machine learning model from the assets folder
   loadmodel() async {
     await Tflite.loadModel(
         model: 'assets/model_unquant.tflite', labels: 'assets/labels.txt');
   }
 
+  //Function for instance disposal
   @override
   void dispose() {
     super.dispose();
   }
 
-  pickimage_camera() async {
-    var image = await imagepicker.getImage(source: ImageSource.camera);
+  //Function to capture an image using the device's camera
+  pickImageCamera() async {
+    var image = await imagepicker.pickImage(source: ImageSource.camera);
     if (image == null) {
       return null;
     } else {
@@ -58,8 +84,9 @@ class _ScanningPage extends State<ScanningPage> {
     detectimage(_image!);
   }
 
-  pickimage_gallery() async {
-    var image = await imagepicker.getImage(source: ImageSource.gallery);
+  //Function to select an image from the device's gallery
+  pickImageGallery() async {
+    var image = await imagepicker.pickImage(source: ImageSource.gallery);
     if (image == null) {
       return null;
     } else {
@@ -70,8 +97,6 @@ class _ScanningPage extends State<ScanningPage> {
 
   @override
   Widget build(BuildContext context) {
-    var h = MediaQuery.of(context).size.height;
-    var w = MediaQuery.of(context).size.width;
     return Scaffold(
       appBar: AppBar(
         title: Text(
@@ -79,79 +104,60 @@ class _ScanningPage extends State<ScanningPage> {
           style: GoogleFonts.roboto(),
         ),
       ),
-      body: Container(
-        height: h,
-        width: w,
-        child: Column(
-          children: [
-            Container(
-              height: 150,
-              width: 150,
-              padding: EdgeInsets.all(10),
-            ),
-            Container(
-                child: Text('Eqipment Checker',
-                    style: GoogleFonts.roboto(
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold,
-                    ))),
-            SizedBox(height: 50),
-            Container(
-              child: Column(
-                children: [
-                  Container(
-                    padding: EdgeInsets.only(left: 10, right: 10),
-                    height: 50,
-                    width: double.infinity,
-                    child: ElevatedButton(
-                        child: Text('Capture',
-                            style: GoogleFonts.roboto(fontSize: 18)),
-                        onPressed: () {
-                          pickimage_camera();
-                        }),
-                  ),
-                  SizedBox(height: 10),
-                  Container(
-                    padding: EdgeInsets.only(left: 10, right: 10),
-                    height: 50,
-                    width: double.infinity,
-                    child: ElevatedButton(
-                        child: Text('Gallery',
-                            style: GoogleFonts.roboto(fontSize: 18)),
-                        onPressed: () {
-                          pickimage_gallery();
-                        }),
-                  ),
-                ],
+      body: Column(
+        children: [
+          // Use SizedBox to add spacing
+          const SizedBox(
+            height: 15,
+            width: 150,
+          ),
+          Column(
+            children: [
+              Container(
+                padding: const EdgeInsets.only(left: 10, right: 10),
+                height: 50,
+                width: double.infinity,
+                child: ElevatedButton(
+                    child: Text('Capture', style: GoogleFonts.roboto(fontSize: 18)),
+                    onPressed: () {
+                      pickImageCamera(); //Trigger image capture from camera
+                    }),
               ),
-            ),
-            loading != true
-                ? Container(
-              child: Column(
-                children: [
-                  Container(
-                    height: 220,
-                    // width: double.infinity,
-                    padding: EdgeInsets.all(15),
-                    child: Image.file(_image!),
-                  ),
-                  _output != null
-                      ? Text(
-                      (_output![0]['label']).toString().substring(2),
-                      style: GoogleFonts.roboto(fontSize: 18))
-                      : Text(''),
-                  _output != null
-                      ? Text(
-                      'Confidence: ' +
-                          (_output![0]['confidence']).toString(),
-                      style: GoogleFonts.roboto(fontSize: 18))
-                      : Text('')
-                ],
+              const SizedBox(height: 10),
+              Container(
+                padding: const EdgeInsets.only(left: 10, right: 10),
+                height: 50,
+                width: double.infinity,
+                child: ElevatedButton(
+                    child: Text('Gallery', style: GoogleFonts.roboto(fontSize: 18)),
+                    onPressed: () {
+                      pickImageGallery(); //Trigger image selection from the gallery
+                    }),
               ),
-            )
-                : Container()
-          ],
-        ),
+            ],
+          ),
+          loading != true
+              ? Column(
+            children: [
+              Container(
+                height: 220,
+                padding: const EdgeInsets.all(15),
+                child: Image.file(_image!),
+              ),
+              _output != null
+                  ? Text(
+                  'Category: $modelPred', //Display predicted category
+                  style: GoogleFonts.roboto(fontSize: 18))
+                  : const SizedBox.shrink(), //Use SizedBox.shrink() to create an empty space
+              _output != null
+                  ? Text(
+                  'Confidence: $modelConf', //Display prediction confidence
+                  style: GoogleFonts.roboto(fontSize: 18))
+                  : const SizedBox.shrink(),
+            ],
+          )
+              : const SizedBox.shrink(),
+        ],
       ),
     );
   }
